b514877e29a7029634c82b4a3ba826857d79ee4a
theMycron <134844972+theMycron@users.noreply.github.com>
2024-05-27 12:59:31 +0300

    removed unnecessary navigational properties for service and user and renamed address to city

---
+++ APProject-ServiceTitan/ServiceTitanBusinessObjects/Migrations/20240527095854_removeNavRenameAddress.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ServiceTitanBusinessObjects.Migrations
+{
+    public partial class removeNavRenameAddress : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ApplicationUserService");
+
+            migrationBuilder.RenameColumn(
+                name: "Address",
+                table: "Users",
+                newName: "City");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "City",
+                table: "Users",
+                newName: "Address");
+
+            migrationBuilder.CreateTable(
+                name: "ApplicationUserService",
+                columns: table => new
+                {
+                    ServicesServiceID = table.Column<int>(type: "int", nullable: false),
+                    TechniciansUserID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ApplicationUserService", x => new { x.ServicesServiceID, x.TechniciansUserID });
+                    table.ForeignKey(
+                        name: "FK_ApplicationUserService_Services_ServicesServiceID",
+                        column: x => x.ServicesServiceID,
+                        principalTable: "Services",
+                        principalColumn: "service_id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ApplicationUserService_Users_TechniciansUserID",
+                        column: x => x.TechniciansUserID,
+                        principalTable: "Users",
+                        principalColumn: "user_id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ApplicationUserService_TechniciansUserID",
+                table: "ApplicationUserService",
+                column: "TechniciansUserID");
+        }
+    }
+}
