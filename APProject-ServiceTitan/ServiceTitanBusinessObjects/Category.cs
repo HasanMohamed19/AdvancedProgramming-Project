8badbb16c186fc00fa05602e3b62b8ce3893cbe6
theMycron <134844972+theMycron@users.noreply.github.com>
2024-05-28 16:23:27 +0300

    updated category display

--- APProject-ServiceTitan/ServiceTitanBusinessObjects/Category.cs
+++ APProject-ServiceTitan/ServiceTitanBusinessObjects/Category.cs
@@ -18,13 +18,17 @@ namespace ServiceTitanBusinessObjects
         [Required]
         [MaxLength(50)]
         [Column("category_name")]
+        [Display(Name = "Category Name")]
         public string CategoryName { get; set; }
 
         [Column("category_description")]
+        [Display(Name = "Description")]
         public string? CategoryDescription { get; set; }
 
+        [Display(Name = "Manager")]
         public int? CategoryManagerId { get; set; }
         [Column("category_manager_id")]
+        [Display(Name = "Manager")]
         public ApplicationUser? CategoryManager { get; set; }
 
         public ICollection<Service> Services { get; } = new HashSet<Service>();
