3def84ac90ccc62115eff4299fa32a7a5c073b1e
Hasan <hzs2003@live.com>
2024-05-28 10:46:01 +0300

    Added baseController to share the GetSource method with all controllers and modified source for logging

--- APProject-ServiceTitan/ServiceTitanWebApp/Controllers/CommentController.cs
+++ APProject-ServiceTitan/ServiceTitanWebApp/Controllers/CommentController.cs
@@ -7,12 +7,13 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using ServiceTitanBusinessObjects;
+using ServiceTitanWebApp.Helpers;
 using ServiceTitanWebApp.ViewModels;
 
 namespace ServiceTitanWebApp.Controllers
 {
     [Authorize]
-    public class CommentController : Controller
+    public class CommentController : BaseController
     {
         private readonly ServiceTitanDBContext _context;
 
@@ -78,7 +79,9 @@ namespace ServiceTitanWebApp.Controllers
             if (ModelState.IsValid)
             {
                 _context.Add(comment);
-                await _context.SaveAsync(User,"WebApp", null);
+                //var sourceRoute = this.ControllerContext.RouteData.Values;
+                //string source = "WebApp: " + sourceRoute["controller"] + ":" + sourceRoute["action"];
+                await _context.SaveAsync(User,GetSourceRoute(), null);
                 TempData["CreateSuccess"] = "Comment Created Successfully";
                 return RedirectToAction(nameof(Index), new { requestId = comment.ServiceRequestId });
             }
@@ -140,7 +143,9 @@ namespace ServiceTitanWebApp.Controllers
                 try
                 {
                     _context.Update(existingComment);
-                    await _context.SaveAsync(User, "WebApp", null);
+                    //var sourceRoute = this.ControllerContext.RouteData.Values;
+                    //string source = "WebApp: " + sourceRoute["controller"] + ":" + sourceRoute["action"];
+                    await _context.SaveAsync(User, GetSourceRoute(), null);
                 }
                 catch (DbUpdateConcurrencyException)
                 {
@@ -206,8 +211,9 @@ namespace ServiceTitanWebApp.Controllers
                     return Forbid();
                 _context.Comments.Remove(comment);
             }
-
-            await _context.SaveAsync(User, "WebApp", null);
+            //var sourceRoute = this.ControllerContext.RouteData.Values;
+            //string source = "WebApp: " + sourceRoute["controller"] + ":" + sourceRoute["action"];
+            await _context.SaveAsync(User, GetSourceRoute(), null);
             TempData["DeleteSuccess"] = "Comment Deleted Successfully";
             return RedirectToAction(nameof(Index), new { requestId = comment.ServiceRequestId });
         }
