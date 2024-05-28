8a8cafeb2dcc0343d1f2631477232e74e06ab983
theMycron <134844972+theMycron@users.noreply.github.com>
2024-05-28 21:19:31 +0300

    added seeds for appusers, might be unnecessary

--- APProject-ServiceTitan/ServiceTitanBusinessObjects/ModelBuilderExtensions.cs
+++ APProject-ServiceTitan/ServiceTitanBusinessObjects/ModelBuilderExtensions.cs
@@ -27,7 +27,14 @@ namespace ServiceTitanBusinessObjects
                 new UserRole { RoleID = 3, RoleName = "Technician" },
                 new UserRole { RoleID = 4, RoleName = "User" }
             );
-
+            modelBuilder.Entity<ApplicationUser>().HasData(
+                new ApplicationUser { UserEmail = "admin@test.com", FirstName = "System", LastName = "Admin", City = "System", PhoneNumber = "00000000", RoleId = 1 },
+                new ApplicationUser { UserEmail = "manager@test.com", FirstName = "Mahdi", LastName = "Marhoon", City = "Zinj", PhoneNumber = "12345678", RoleId = 2 },
+                new ApplicationUser { UserEmail = "tech1@test.com", FirstName = "Qassim", LastName = "Meer", City = "Riffa", PhoneNumber = "12345678", RoleId = 3 },
+                new ApplicationUser { UserEmail = "tech2@test.com", FirstName = "Salman", LastName = "Mandi", City = "Isa Town", PhoneNumber = "12345678", RoleId = 3 },
+                new ApplicationUser { UserEmail = "user1@test.com", FirstName = "Yousif", LastName = "Alhawaj", City = "Zinj", PhoneNumber = "12345678", RoleId = 4 },
+                new ApplicationUser { UserEmail = "user2@test.com", FirstName = "Malik", LastName = "Yaseen", City = "Cave", PhoneNumber = "12345678", RoleId = 4 }
+            );
         }
     }
 }
